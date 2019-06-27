d9ec669b2845aaa914228423a9a6c1bb167abbab
Patrick <pdownton@pbigordon.com>
2019-06-27 10:32:00 -0500

    Initial Commit

---
+++ Todoey/ToDoListViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Patrick Downton on 6/27/19.
+//  Copyright © 2019 Patrick Downton. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    //Mark - Tableview datasource Methods
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    //Mark - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       // print (itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+          tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+         else {
+          tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+            }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+        
+    }
+
+}
+
